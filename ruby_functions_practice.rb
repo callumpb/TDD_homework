afc4a0a3cf331d825a119d09fd594f1615eecfd9
Callum <callumpb@hotmail.com>
2017-09-26 17:05:23 +0100

    passed test_number_to_full_name

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -42,3 +42,16 @@ def number_to_full_month_name(input_number)
   end
   return month
 end
+
+
+def number_to_short_month_name(input_number)
+  month = case input_number
+  when 1
+    "Jan"
+  when 3
+    "Mar"
+  when 9
+    "Sep"
+  end
+return month
+end
